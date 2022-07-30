01ba41895fe7fd50225d6a14a02e6eb74958c968
Samuel Angulo <samuel.angulo_c@mail.udp.cl>
2022-07-29 21:47:02 -0400

    agregando gitignore

--- home/views.py
+++ home/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Esta es la vista del blog.")
+    return HttpResponse("THIS IS THE MAIN")
